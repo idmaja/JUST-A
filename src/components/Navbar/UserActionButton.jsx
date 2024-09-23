83c2d5a1ec0b25d00dede6b0090bf4bc73f61d26
idmaja <672021186@student.uksw.edu>
2024-09-23 13:59:16 +0700

    add: website logo; modified: UserActionButton

--- src/components/Navbar/UserActionButton.jsx
+++ src/components/Navbar/UserActionButton.jsx
@@ -8,16 +8,18 @@ const UserActionButton = async() => {
     const user = await authUserSession()
 
     const actionLabel = user ? 
-                        <SignOut size={32} className="text-color-red"/> 
+                        <SignOut size={32} className="text-color-red" weight="bold" alt="Sign out"/> 
+                        // <><h5 className="text-color-red w-24 text-center text-lg transition-all justify-center items-center">Sign Out</h5></>
                         : 
-                        <SignIn size={32} className=" text-color-primary"/>
+                        <SignIn size={32} className=" text-color-primary" alt="Sign in" weight="bold"/>
+                        // <><h5 className="text-color-primary w-24 text-center text-lg transition-all justify-center ">Sign In</h5></>
     const actionURL = user ? "/api/auth/signout" : "/api/auth/signin"
     return (
         <div className="flex justify-between gap-2">
             {
                 user ? 
                 <Link href="/users/dashboard" className=" bg-color-dark text-color-primary hover:bg-color-secondary rounded-3xl">
-                    <Image src={user?.image} alt="Dashboard" width={80} height={80} className="rounded-3xl"/>
+                    <Image src={user?.image} alt="Dashboard" width={90} height={90} className="rounded-3xl"/>
                 </Link> 
                     : null
             } 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import InputSearch from "./InputSearch";
 import { ArrowsInCardinal } from "@phosphor-icons/react/dist/ssr";
 import UserActionButton from "./UserActionButton";
@@ -8,9 +9,10 @@ const Navbar = () => {
     <header className="bg-color-accent">
       <div className="flex flex-col justify-between gap-2 p-4 md:items-center md:flex-row">
         <Link href="/" className="text-2xl font-bold">
-          <div className="flex items-center gap-2">
+          <div className="flex items-center">
             {/* JUST-A */}
-            <ArrowsInCardinal size={60} className="text-color-primary"/>
+            <Image src="/just-a-logo.svg" alt="..." width={500} height={500} className="object-cover w-full h-28 rounded"/>
+            {/* <ArrowsInCardinal size={60} className="text-color-primary"/> */}
             {/* <span className="font-bold text-color-primary">JUST-A</span> */}
           </div>
         </Link>
