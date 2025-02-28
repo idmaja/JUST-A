89f4ecdcff0837495b4ec7863c8c5eab02aca0e5
idmaja <dhimasm999@gmail.com>
2025-02-28 17:15:30 +0700

    modified UserActionButton

--- src/components/Navbar/UserActionButton.jsx
+++ src/components/Navbar/UserActionButton.jsx
@@ -3,14 +3,6 @@ import UserActionButtonClient from "./UserActionButtonClient";
 
 const UserActionButton = async () => {
   const user = await authUserSession();
-  if (!user) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <p className="text-xl">You must be logged in to view this page.</p>
-      </div>
-    );
-  }
-
   return <UserActionButtonClient user={user} />;
 };
 
